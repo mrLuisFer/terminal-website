933a26658672155db3cd0a5fbcb76122236e2e06
mrLuisFer <mrluisfeer@gmail.com>
2022-06-21 22:51:30 -0500

    formatting the open command

--- src/ts/events/open.ts
+++ src/ts/events/open.ts
@@ -7,18 +7,22 @@ interface OpenProps extends CommonEventProps {
 export default function open({liElement, sectionElement, value}: OpenProps) {
   liElement.insertAdjacentElement("afterend", sectionElement)
   const eventValue: string[] = value.split(" ")
-  const regex = new RegExp('https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()!@:%_\\+.~#?&\\/\\/=]*)')
+  const httpsRegex = new RegExp('https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()!@:%_\\+.~#?&\\/\\/=]*)')
 
+  let html = ``
   if (eventValue[1] !== undefined) {
     const url: string = eventValue[1]
-    if (url.match(regex)) {
+    if (url.match(httpsRegex)) {
       window.open(url, "_blank")
     } else if (eventValue[1] === "--help" || eventValue[1] === "help") {
-      console.log("help parameter")
+      html = `<p class="command__content command__content--info">Help</p>`
+      liElement.insertAdjacentHTML("afterend", html)
     } else {
-      console.log("url is not valid")
+      html = `<p class="command__content command__content--info">url is not valid</p>`
+      liElement.insertAdjacentHTML("afterend", html)
     }
   } else {
-    console.log("need provide an url")
+    html = `<p class="command__content command__content--info">need provide an url</p>`
+    liElement.insertAdjacentHTML("afterend", html)
   }
 }
